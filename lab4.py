6afb442c5dedac848bad12d8a9694b42413d2354
Sarah Jomaa <sljomaa@umich.edu>
2017-09-28 15:58:21 -0400

    modified lab4.py

---
+++ lab4.py
@@ -0,0 +1,16 @@
+import re
+hand = open('mbox-short.txt')
+
+with open("mbox-short.txt", "r") as f:
+	f = f.readlines()
+	for line in f:
+		if re.search('From', line):
+			print(line)
+			numbers = re.findall('[0-9]+', line)
+			print (numbers)
+			name = re.findall('(\S*)@', line)
+			print (name)
+
+
+
+
